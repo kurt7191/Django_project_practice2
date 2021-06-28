4bada56a46fa675ce283543e86d7022b0a32ac1d
eunjun <qw71913281@gmail.com>
2021-06-29 06:11:48 +0900

    update

--- restproject/shareRes/views.py
+++ restproject/shareRes/views.py
@@ -5,9 +5,22 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shareRes/index.html')
-    # return HttpResponse('index')
+    # return HttpResponse("index")
+
+def restaurantDetail(request):
+    return render(request, 'shareRes/restaurantDetail.html')
+    # return HttpResponse("restaurantDetail")
+
+def restaurantCreate(request):
+    return render(request, 'shareRes/restaurantCreate.html')
+    # return HttpResponse("restaurantCreate")
+
+def categoryCreate(request):
+    return render(request, 'shareRes/categoryCreate.html')
+    # return HttpResponse("categoryCreate")
 
 
+    
 # def restaurantDetail(request):
 #     return HttpResponse('restaurantDetail')
 
